514bb287288a7eac5955d90a0031b3445168f88c
johnny-soga <johnny@plasmism.com>
2024-12-12 11:31:44 +0900

    ogp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,26 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Lean Designer | 開発専門のハイエンドUI/UXソリューション',
   description: 'ピンポイントの開発課題を必要な分だけスペシャリストのデザイナーに発注ができる、UI/UXデザインソリューション',
+  openGraph: {
+    title: 'Lean Designer | 開発専門のハイエンドUI/UXソリューション',
+    description: 'ピンポイントの開発課題を必要な分だけスペシャリストのデザイナーに発注ができる、UI/UXデザインソリューション',
+    images: [
+      {
+        url: '/ogp.png',
+        width: 1200,
+        height: 630,
+        alt: 'Lean Designer',
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Lean Designer | 開発専門のハイエンドUI/UXソリューション',
+    description: 'ピンポイントの開発課題を必要な分だけスペシャリストのデザイナーに発注ができる、UI/UXデザインソリューション',
+    images: ['/ogp.png'],
+  },
 };
 
 export default function RootLayout({
