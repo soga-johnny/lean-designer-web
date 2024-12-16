fc6752e51d5996a2eeb122a9b6a5ff5a80dfeac1
johnny-soga <johnny@plasmism.com>
2024-12-16 15:56:49 +0900

    fix-clatiy02

--- src/components/ClarityScript.tsx
+++ src/components/ClarityScript.tsx
@@ -2,10 +2,20 @@ import Script from 'next/script'
 
 export default function ClarityScript() {
   return (
-    <Script
-      id="microsoft-clarity"
-      strategy="afterInteractive"
-      src="https://www.clarity.ms/tag/pedus1ve4e"
-    />
+    <>
+      <Script
+        id="microsoft-clarity-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "pedus1ve4e");
+          `
+        }}
+      />
+    </>
   )
 } 
